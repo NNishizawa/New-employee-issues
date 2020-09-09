d8b585eb9f510ccd9f4f46bc29afe447a2bf578f
NNishizawa <n-nishizawa@clinks.jp>
2020-09-09 11:08:00 +0900

    課題4 ボタンでWindowクラスのライフサイクルを呼べるように修正

--- src/app/src/main/java/com/example/newemployeeissues/MainActivity.kt
+++ src/app/src/main/java/com/example/newemployeeissues/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.newemployeeissues
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var window = Window()
+
+        btn_onCreateWindow.setOnClickListener {
+            var newWindow = Window()
+            window.onCreateWindow(newWindow)
+        }
+
+        btn_onDestroyWindow.setOnClickListener {
+            window.onDestroyWindow()
+        }
+
+        btn_getTopWindow.setOnClickListener {
+            val topWindow = window.getTopWindow()
+        }
     }
 }
