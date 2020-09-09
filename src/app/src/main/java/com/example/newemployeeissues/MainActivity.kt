f1ba2aedeaac2afa4308af64192fb772d7c6c99c
NNishizawa <n-nishizawa@clinks.jp>
2020-09-09 17:33:58 +0900

    課題9 実行確認できるように修正

--- src/app/src/main/java/com/example/newemployeeissues/MainActivity.kt
+++ src/app/src/main/java/com/example/newemployeeissues/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.newemployeeissues
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var state :State = State.IDLE
@@ -10,8 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var event : Event = TODO("イベントを取得")
+        btn_event_play.setOnClickListener {
+            eventChg(Event.BUTTON_PLAY)
+        }
 
+        btn_event_record.setOnClickListener {
+            eventChg(Event.BUTTON_RECORD)
+        }
+
+        btn_event_stop.setOnClickListener {
+            eventChg(Event.BUTTON_STOP)
+        }
+    }
+
+    private fun eventChg(event: Event) {
         when(event) {
             Event.BUTTON_PLAY -> { eventPlayButton() }
             Event.BUTTON_RECORD -> { eventRecordButton() }
@@ -19,35 +32,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun eventPlayButton() {
+    private fun eventPlayButton() {
         when (state) {
-            State.IDLE -> { TODO("再生開始") }
+            State.IDLE -> {
+                // TODO("再生開始")
+                System.out.println("再生開始")
+            }
             State.PLAYING -> {}
             State.RECORDING -> {
-                TODO("録画停止")
-                TODO("再生開始")
+                // TODO("録画停止")
+                System.out.println("録画停止")
+
+                // TODO("再生開始")
+                System.out.println("再生開始")
             }
         }
         state = State.PLAYING
     }
 
-    fun eventRecordButton() {
+    private fun eventRecordButton() {
         when (state) {
-            State.IDLE -> { TODO("録画開始") }
+            State.IDLE -> {
+                // TODO("録画開始")
+                System.out.println("録画開始")
+            }
             State.PLAYING -> {
-                TODO("再生停止")
-                TODO("録画開始")
+                // TODO("再生停止")
+                System.out.println("再生停止")
+
+                // TODO("録画開始")
+                System.out.println("録画開始")
             }
             State.RECORDING -> {}
         }
         state = State.RECORDING
     }
 
-    fun eventStopButton() {
+    private fun eventStopButton() {
         when (state) {
             State.IDLE -> {}
-            State.PLAYING -> { TODO("再生停止") }
-            State.RECORDING -> { TODO("録画停止") }
+            State.PLAYING -> {
+                // TODO("再生停止")
+                System.out.println("再生停止")
+            }
+            State.RECORDING -> {
+                // TODO("録画停止")
+                System.out.println("録画停止")
+            }
         }
         state = State.IDLE
     }
